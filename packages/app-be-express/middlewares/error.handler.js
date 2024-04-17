ec11afb40f4570c970355a4e3961903c3dc22b5e
jason <js.solarte29@gmail.com>
2024-04-17 13:08:17 -0500

    fix(packages/app-be-express): minimize error info

--- packages/app-be-express/middlewares/error.handler.js
+++ packages/app-be-express/middlewares/error.handler.js
@@ -19,8 +19,7 @@ function ormErrorHandler(err, req, res, next) {
   if (err instanceof ValidationError) {
     res.status(409).json({
       statusCode: 409,
-      message: err.name,
-      errors: err.errors,
+      message: 'Error in data',
     });
   }
   next(err);
