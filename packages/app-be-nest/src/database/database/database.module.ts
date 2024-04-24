856cbbc2b62b2b6d91e58a0fe2b80ba644374865
jason <js.solarte29@gmail.com>
2024-04-24 10:47:49 -0500

    feat(packages/app-be-nest): db connection
    db connection with postgres throughout typeorm

---
+++ packages/app-be-nest/src/database/database/database.module.ts
@@ -0,0 +1,35 @@
+import { Global, Module } from "@nestjs/common";
+import { ConfigType } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import config from "src/config/config";
+
+@Global()
+@Module({
+	imports: [
+		TypeOrmModule.forRootAsync({
+			inject: [config.KEY],
+			useFactory: (configService: ConfigType<typeof config>) => {
+				const {
+					host,
+					password,
+					user: username,
+					port,
+					name: database,
+				} = configService.database;
+
+				return {
+					type: "postgres",
+					host,
+					port,
+					username,
+					password,
+					database,
+					// synchronize: true,
+					autoLoadEntities: true,
+				};
+			},
+		}),
+	],
+	exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
