0a3ae676c2e86ab818304d903b4555b02d2ece5b
jason <js.solarte29@gmail.com>
2024-04-24 10:44:43 -0500

    feat(packages/app-be-nest): swagger documentation

--- packages/app-be-nest/src/main.ts
+++ packages/app-be-nest/src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		}),
+	);
+
+	const config = new DocumentBuilder()
+		.setTitle("Social red")
+		.setDescription("Social red API")
+		.setVersion("1.0")
+		.addTag("social red")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api/document", app, document);
+	app.setGlobalPrefix("api/v1");
+	await app.listen(3000);
 }
 bootstrap();
